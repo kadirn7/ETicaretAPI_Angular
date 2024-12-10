44a7fbc94482844c536c0132089f97a3ad4934f8
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-12-10 23:46:00 +0300

    Entityler eklendi ve Database oluşturuldu. appsettingsjson ile bağlantı farklı sunumla yapıldı bu yeni bir bilgi.

---
+++ Infrastructure/ETicaretAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using ETicaretAPI.Persistence.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretDbContext> //Power shell ile migration oluşturmak gerekirse.
+    {                                                                                        // dotnet ef migrations add mig_1 gibi 
+        public ETicaretDbContext CreateDbContext(string[] args)
+        {
+            
+
+            DbContextOptionsBuilder<ETicaretDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
